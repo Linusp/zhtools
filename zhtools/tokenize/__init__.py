c572f2c8ac23f3f8574234dcc9d24e3d9f5f86cf
Linusp <linusp1024@gmail.com>
2019-06-30 16:39:26 +0800

    update exportings

--- zhtools/tokenize/__init__.py
+++ zhtools/tokenize/__init__.py
@@ -1,3 +1,25 @@
-from .base import register_tokenizer, Tokenizer, get_tokenizer
+from .token import Token
+from .base import (
+    register_tokenizer,
+    Tokenizer,
+    get_tokenizer,
+    NgramTokenizer,
+    SpaceTokenizer,
+)
+from .ext import (
+    JiebaTokenizer,
+    PKUTokenizer,
+    CoreNLPTokenizer,
+)
 
-__all__ = ["register_tokenizer", "Tokenizer", "get_tokenizer"]
+__all__ = [
+    'Token',
+    'register_tokenizer',
+    'Tokenizer',
+    'get_tokenizer',
+    'NgramTokenizer',
+    'SpaceTokenizer',
+    'JiebaTokenizer',
+    'PKUTokenizer',
+    'CoreNLPTokenizer',
+]
