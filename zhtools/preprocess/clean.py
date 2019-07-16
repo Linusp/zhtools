4533435b01c4bbffb7f8b0602db8376b9ba25bb6
Linusp <linusp1024@gmail.com>
2019-07-16 08:29:36 +0800

    fix `zhtools.preprocess.to_halfwidth` and add logging

--- zhtools/preprocess/clean.py
+++ zhtools/preprocess/clean.py
@@ -1,4 +1,8 @@
+import logging
 import unicodedata
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 def to_halfwidth(text):
@@ -9,9 +13,12 @@ def to_halfwidth(text):
             result += ' '
         elif not name or name.find('FULLWIDTH') != 0:
             result += char
-        else:
+        elif name is not None:
             new_name = name.replace('FULLWIDTH', '').strip()
             new_char = unicodedata.lookup(new_name)
             result += new_char
+        else:
+            LOGGER.warning("can't get name from unicodedata: `%s`(%x)", char, ord(char))
+            result += char
 
     return result
